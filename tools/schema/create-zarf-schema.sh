20df2eff4df14c85a9f5ea6f84f2a7da7831397f
Austin Abro <AustinAbro321@gmail.com>
2024-08-15 14:25:31 +0000

    fix script
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>

--- tools/schema/create-zarf-schema.sh
+++ tools/schema/create-zarf-schema.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 add_yaml_extensions() {
   local src=$1
   local dst=$2
@@ -19,12 +21,12 @@ add_yaml_extensions() {
   ' "$src" > "$dst"
 }
 
-go run schema/src/main.go v1alpha1 > "zarf_package_v1alpha1.schema.json"
-go run schema/src/main.go v1beta1 > "zarf_package_v1beta1.schema.json"
+go run "$SCRIPT_DIR/main.go" v1alpha1 > "zarf_package_v1alpha1.schema.json"
+go run "$SCRIPT_DIR/main.go" v1beta1 > "zarf_package_v1beta1.schema.json"
 
-add_yaml_extensions "zarf_package_v1alpha1.schema.json" "../../zarf.schema.json"
-add_yaml_extensions "zarf_package_v1alpha1.schema.json" "../../schema/zarf_package_v1alpha1.schema.json"
-add_yaml_extensions "zarf_package_v1beta1.schema.json" "../../schema/zarf_package_v1beta1.schema.json"
+add_yaml_extensions "zarf_package_v1alpha1.schema.json" "$SCRIPT_DIR/../../zarf.schema.json"
+add_yaml_extensions "zarf_package_v1alpha1.schema.json" "$SCRIPT_DIR/../../schema/zarf_package_v1alpha1.schema.json"
+add_yaml_extensions "zarf_package_v1beta1.schema.json" "$SCRIPT_DIR/../../schema/zarf_package_v1beta1.schema.json"
 
 rm zarf_package_v1alpha1.schema.json
 rm zarf_package_v1beta1.schema.json
